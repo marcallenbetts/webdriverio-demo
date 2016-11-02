8f4e590695fc99da6cf5ac4a10d8a1ec9fda64d4
WillLuce <luce.williamt@gmail.com>
2016-11-01 20:21:25 -0600

    WebdriverIO Example implementation

---
+++ pages/login.page.ts
@@ -0,0 +1,18 @@
+
+class Login_Page {
+
+    public get username()  { return browser.element('#username') }
+    public get password()  { return browser.element('#password') }
+    public get form()      { return browser.element('#login') }
+    public get flash()     { return browser.element('#flash') }
+
+    public open() {
+        browser.url('/login')
+    }
+    public submit() {
+        this.form.submitForm()
+    }
+
+}
+const LoginPage = new Login_Page()
+export default LoginPage
